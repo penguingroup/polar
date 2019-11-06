9d8b278d075e46807b238c3066b4b1964cdde348
karlkartli <karlkartli@tencent.com>
2019-11-06 10:35:43 +0800

    兼容前端，增加跨域允许

--- global/param.go
+++ global/param.go
@@ -15,6 +15,6 @@ type City struct {
 type NewsRequest struct {
 	City     string `json:"city" form:"city" required:"true"`
 	Category string `json:"category" form:"category" required:"true"`
-	Page     int64  `json:"page" form:"page" required:"true"`
+	Page     int64  `json:"page" form:"page" required:"true" default:"0"`
 	Size     int64  `json:"size" form:"size" required:"true" default:"10"`
 }
